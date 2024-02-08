df6fe4070f75b46c127417f9521a94fd4dd2eecf
SivanLevi100 <sivanlevi94@gmail.com>
2024-02-08 23:16:08 +0200

    adding date frame of videos.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import streamlit as st
 #import mymodel as my
-from page2 import *
+import streamlit as st
+#from ProjectSmartGuard.pages.page2 import *
 
-#st.write("My name is: Sivan")
+
+
+
+
 st.title("Welcome to the SmartGuard application")
+
+st.sidebar.success("select page above")
+
+
 
 b = st.button("click here")
 if b:
@@ -25,31 +27,6 @@ video_file = open('V_1.mp4', 'rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
 
-
-
-st.button(
-    label='CLASSIFICATION',
-    key='classify',
-    help='Click to classify',
-    on_click=classify_video,
-    args=(user_video,),
-    kwargs={
-        'border_radius': '10px',
-        'padding': '15px 32px',
-        'font_size': '20px',
-        'font_weight': 'bold',
-        'background_color': '#0066CC',
-        'color': '#ffffff'
-    }
-)
-
-
-
-def go_to_page2():
-    st.experimental_singleton().clear()
-    page2()
-
-st.button('Go to Page 2', on_click=go_to_page2)
 
 
 x = st.slider("Select a value")
